1af35b4601d043e0ae7ad8cce3dd0b289436e6b8
mitun94 <tohidulalammitun@gmail.com>
2017-01-22 01:47:20 +0600

    no message

--- rango/urls.py
+++ rango/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url,include
 from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
 ]
