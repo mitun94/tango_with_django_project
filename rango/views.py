774154afd479865fc0fb81014acac6a96a8e0643
imranctg16 <imranhossain16.ctg@gmail.com>
2017-01-22 11:41:50 +0600

    Replaced Add_category function with mine

--- rango/views.py
+++ rango/views.py
@@ -26,13 +26,22 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context_dict)
 
 def add_category(request):
-    form = CategoryForm()
+    # A HTTP POST?
     if request.method == 'POST':
         form = CategoryForm(request.POST)
+        # Have we been provided with a valid form?
         if form.is_valid():
+            # Save the new category to the database.
             form.save(commit=True)
+            # Now call the index() view.
+            # The user will be shown the homepage.
             return index(request)
         else:
+            # The supplied form contained errors - just print them to the terminal.
             print(form.errors)
-    return render(request,'rango/add_category.html',{'form':form})
-
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
